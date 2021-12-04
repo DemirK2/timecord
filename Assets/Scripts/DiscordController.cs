3d6e55f351016f83fcc3d06542769cd740abf3ab
DemirK2 <demirk2@pokenix.com>
2021-12-04 22:30:07 +0300

    Version 1.0

---
+++ Assets/Scripts/DiscordController.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using UnityEngine;
+
+public class DiscordController : MonoBehaviour
+{
+    public GameController game;
+    public Discord.Discord discord;
+    public bool connected;
+
+    void Start()
+    {
+        discord = new Discord.Discord(916744715192377395, (ulong)CreateFlags.Default);
+        connected = true;
+    }
+
+    void Update()
+    {
+        if (connected)
+        {
+            discord.RunCallbacks();
+        }
+    }
+
+    public void UpdateActivity(Activity activity)
+    {
+        var activityManager = discord.GetActivityManager();
+        activityManager.UpdateActivity(activity, (res) =>
+        {
+            if (res == Result.Ok)
+            {
+                Debug.Log("[Discord] Updated.");
+            }
+            else
+            {
+                Debug.LogWarning("[Discord] Couldn't update!");
+            }
+        });
+    }
+
+    public void Connect()
+    {
+        discord = new Discord.Discord(916744715192377395, (ulong)CreateFlags.Default);
+        connected = true;
+    }
+
+    public void Dispose()
+    {
+        discord.Dispose();
+        connected = false;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,75 @@
+﻿using Discord;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public DiscordController discord;
+    public long time = 0;
+    public GameObject status;
+
+    void Start()
+    {
+        var activity = new Activity { };
+        activity.Details = $"Time: {DateTime.Now.ToString("HH:mm")}";
+        if (status.GetComponent<Text>().text == "Status: AFK" || status.GetComponent<Text>().text == "Status: Sleeping")
+        {
+            activity.State = status.GetComponent<Text>().text;
+        }
+        discord.UpdateActivity(activity);
+    }
+
+    void Update()
+    {
+        if (time == 0) time = DateTimeOffset.Now.ToUnixTimeSeconds() + 60;
+        if (DateTimeOffset.Now.ToUnixTimeSeconds() >= time)
+        {
+            time = time + 60;
+            var activity = new Activity { };
+            activity.Details = $"Time: {DateTime.Now.ToString("HH:mm")}";
+            if (status.GetComponent<Text>().text == "Status: AFK" || status.GetComponent<Text>().text == "Status: Sleeping")
+            {
+                activity.State = status.GetComponent<Text>().text;
+            }
+            discord.UpdateActivity(activity);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        discord.Dispose();
+    }
+
+    public void ChangeStatus()
+    {
+        int currentStatus = 0;
+        switch (status.GetComponent<Text>().text)
+        {
+            case "Status: Normal":
+                currentStatus = 1;
+                break;
+            case "Status: AFK":
+                currentStatus = 2;
+                break;
+            case "Status: Sleeping":
+                currentStatus = 3;
+                break;
+        }
+        if (currentStatus == 1) status.GetComponent<Text>().text = "Status: AFK";
+        if (currentStatus == 2) status.GetComponent<Text>().text = "Status: Sleeping";
+        if (currentStatus == 3) status.GetComponent<Text>().text = "Status: Normal";
+        var activity = new Activity { };
+        activity.Details = $"Time: {DateTime.Now.ToString("HH:mm")}";
+        if (status.GetComponent<Text>().text == "Status: AFK" || status.GetComponent<Text>().text == "Status: Sleeping")
+        {
+            activity.State = status.GetComponent<Text>().text;
+        }
+        discord.UpdateActivity(activity);
+    }
+
+    public void OpenURL(string URL)
+    {
+        Application.OpenURL(URL);
+    }
+}
